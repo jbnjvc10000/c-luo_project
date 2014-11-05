f1f86dc1b76862e66fb1113565ce4d5b80a71089
njzhenghao <njzhenghao@126.com>
2014-11-05 12:28:27 +0800

    写完了界面，但是还没解决好输入界面输入一长串字符的bug。

--- c-luo_project/main.cpp
+++ c-luo_project/main.cpp
@@ -1,1 +1,90 @@
 #include"main.h"
+void employment::edit()
+{
+	system("cls");
+	cout << "开发中";
+	for (int pp = 1; pp <= 6; pp++)
+	{
+		cout << ".";
+		Sleep(100);
+	}
+	system("cls");
+}
+void employment::search()
+{	
+	system("cls");
+	cout << "开发中";
+	for (int pp = 1; pp <= 6; pp++)
+	{
+		cout << ".";
+		Sleep(100);
+	}
+	system("cls");
+}
+void employment::statistics()
+{
+	system("cls");
+	cout << "开发中";
+	for (int pp = 1; pp <= 6; pp++)
+	{
+		cout << ".";
+		Sleep(100);
+	}
+	system("cls");
+}
+void main()
+{
+	int menu_num;
+	employment company;
+	char system_y_n = 'y';
+	while (system_y_n == 'y')
+	{
+		char selecte;
+		cout << "****************************************************************" << endl;
+		cout << "*                                                              *" << endl;
+		cout << "*                    欢迎使用工资管理系统                      *" << endl;
+		cout << "*                                                              *" << endl;
+		cout << "*            1。员工编辑                2。员工查询            *" << endl;
+		cout << "*                                                              *" << endl;
+		cout << "*            3。工资统计                4。系统退出            *" << endl;
+		cout << "*                                                              *" << endl;
+		cout << "*                         请输入相应编号：                     *" << endl;
+		cout << "*                                                              *" << endl;
+		cout << "****************************************************************" << endl;
+		cin >> selecte;
+		if (selecte >= 49 && selecte <= 52)
+		{
+			switch (selecte)
+			{
+			case 49:
+				company.edit();
+				break;
+			case 50:
+				company.search();
+				break;
+			case 51:
+				company.statistics();
+				break;
+			case 52:
+				system_y_n = 'n';
+				system("cls");
+				cout << "正在退出程序";
+				for (int pp = 1; pp <= 6; pp++)
+				{
+					Sleep(60);
+					cout << ".";		
+				}
+				break;
+			default:
+				system("cls");
+				break;
+			}
+		}
+		else
+		{
+			cout << "输入错误，请重新输入……" << endl;
+			Sleep(1000);
+			system("cls");
+		}
+	}
+}
--- c-luo_project/main.h
+++ c-luo_project/main.h
@@ -1,2 +1,13 @@
 #include <iostream>
+#include<cmath>
+#include<cstring>
+#include <Windows.h>
 using namespace std;
+class employment
+{
+public:
+	void edit();
+	void search();
+	void statistics();
+};
+void main();
